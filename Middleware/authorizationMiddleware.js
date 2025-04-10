7f35aab2e3107487aa0cfb5497109df79647335f
a.maher1506@gmail.com <68017554+Maher1506@users.noreply.github.com>
2025-04-10 14:20:50 +0200

    added authorization middleware

---
+++ Middleware/authorizationMiddleware.js
@@ -0,0 +1,25 @@
+// middleware function to check if the role of the request is authorized or not
+function authorizeUser(roles) {
+    try {
+        return (req, res, next) => {
+            // to ensure that the req.user exists (and req.user.role)
+            if (!req.user || !req.user.role) {
+                return res.status(401).json({ message: "Unauthorized: no user info found"});
+            }
+        
+            // get user role from request 
+            const userRole = req.user.role;
+            // check if user role is in the list of roles authorized or not
+            if (!roles.includes(userRole)) {
+                // not authorized
+                return res.status(403).json({ message: "Unauthorized Access" });
+            }
+            next(); // call next middleware
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Authorization Error", error: err.message });
+    }
+}
+
+// export the middleware
+module.exports = authorizeUser;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../Controllers/userController');
+const authorizeUser = require('../Middleware/authorizationMiddleware');
 
 const router = express.Router();
 
-// get all users route
-router.get('/api/v1/users', userController.getAllUsers);
+// get all users route (Admin)
+router.get('/api/v1/users', authorizeUser(['Admin']), userController.getAllUsers);
 
 // export router
 module.exports = router;
