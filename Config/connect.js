4863a6c5e70f8d00b01d767a3fa12401fa8f8049
a.maher1506@gmail.com <68017554+Maher1506@users.noreply.github.com>
2025-04-10 12:05:31 +0200

    connected to database

--- Config/connect.js
+++ Config/connect.js
@@ -2,3 +2,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+const url = process.env.MONGO_URL;
+
+mongoose
+    .connect(url)
+    .then(() => {console.log("Connected to DB")})
+    .catch((err) => {console.log(err)});
+
