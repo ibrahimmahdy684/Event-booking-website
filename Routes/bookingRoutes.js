f3dce11ff2c4ba56f469a174f3958ff530b644be
ammarmaged <ammarmaged1937@gmail.com>
2025-04-11 16:13:35 +0200

    Book a ticket for event

--- Routes/bookingRoutes.js
+++ Routes/bookingRoutes.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const bookingController = require("../Controllers/bookingController");
+
+// load middlewares
+const authorizeUser = require('../Middleware/authorizationMiddleware');
+const authenticateUser = require('../Middleware/authenticationMiddleware');
+
+const router = express.Router();
+
+// Booking ticket for event for standard user
+router.post(
+    '/api/v1/bookings',
+    authenticateUser,
+    authorizeUser(['Standard User']),
+    bookingController.bookTicketsForEvent
+)
