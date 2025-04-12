d02498b6138f42f4f25cf80dfe3848ae780f485a
a.maher1506@gmail.com <68017554+Maher1506@users.noreply.github.com>
2025-04-12 22:26:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,4 @@
-//const app = require('./app');
-
-// load packages
-require('dotenv').config();
-const express = require('express');
-const cookieParser = require('cookie-parser');
-
-// load db and routes
-const connectDB = require('./Config/dbConnect');
-const userRoutes = require('./Routes/userRoutes');
-const authRoutes = require('./Routes/authRoutes');
-const eventRoutes = require('./Routes/eventRoutes');
-const bookingRoutes = require('./Routes/bookingRoutes');
-
-connectDB();
-const app = express();
-
-// use middleware to parse JSON from requests
-app.use(express.json());
-// use middleware to parse cookies from requests
-app.use(cookieParser());
-
-// use all routes
-app.use(userRoutes);
-app.use(authRoutes);
-app.use(eventRoutes);
-app.use(bookingRoutes);
+const app = require('./app');
 
 // get port number or default to 5000
 const PORT = process.env.PORT || 5000;
