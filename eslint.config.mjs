7fe28037d4872c4105ee8ea7385a62bc8898cd6d
a.maher1506@gmail.com <68017554+Maher1506@users.noreply.github.com>
2025-05-01 15:59:56 +0300

    code and prettier refactor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 
-
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
