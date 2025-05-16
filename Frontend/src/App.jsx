e4ecb1baf519c11803a32fa6b5e01b44864543aa
a.maher1506@gmail.com <68017554+Maher1506@users.noreply.github.com>
2025-05-16 16:12:40 +0300

    added navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
+    <Router>
       <div className="App">
+        <Navbar />
+
         <div className="content">
-          <h1>App Component</h1>
-          <button>hey</button>
+          <Routes>
+            <Route path="/" element={<h1>Home Page</h1>} />
+            <Route path="/login" element={<h1>Login Page</h1>} />
+            <Route path="/register" element={<h1>Register Page</h1>} />
+          </Routes>
         </div>
       </div>
-    </>
+    </Router>
   );
 }
 
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
+import "../styles/Navbar.css";
+
 const Navbar = () => {
   return (
-    <nav>
-      <h2>Nav bar</h2>
-      <a href="">Home</a>
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        <h2>Event Booking</h2>
+      </Link>
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
     </nav>
   );
 };
