9b6d7da6e194b5b8a6180c36e81e8a0ca6b4e90f
jootaha <yousseftaha823@gmail.com>
2025-05-17 21:52:29 +0300

    added loading spinner

---
+++ Frontend/src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import loadingSVG from "../assets/loading.svg"; // adjust the path based on location
+
+const LoadingSpinner = () => {
+  return (
+    <div style={{ textAlign: "center", margin: "2rem" }}>
+      <img src={loadingSVG} alt="Loading..." width={50} />
+      <p>Loading...</p>s
+    </div>
+  );
+};
+
+export default LoadingSpinner;
